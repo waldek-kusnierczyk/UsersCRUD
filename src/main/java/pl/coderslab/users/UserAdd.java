cee1b9762a6ac29d11ed3009188fbff32bf27969
Waldek <waldek04@o2.pl>
2020-11-08 13:55:22 +0100

    dziala dodawanie

--- src/main/java/pl/coderslab/users/UserAdd.java
+++ src/main/java/pl/coderslab/users/UserAdd.java
@@ -1,37 +1,47 @@
 package pl.coderslab.users;
 
+import pl.coderslab.entity.User;
+import pl.coderslab.entity.UserDao;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.Arrays;
 
 @WebServlet("/user/add")
 public class UserAdd extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        UserDao userDao = new UserDao();
+//        request.setAttribute("users", userDao.findAll());
 
-    @Override
-    protected void doGet(HttpServletRequest request,
-                         HttpServletResponse response) throws IOException {
-
-        request.setCharacterEncoding("utf-8");
-        response.setCharacterEncoding("utf-8");
-        response.setContentType("text/html");
-
-        PrintWriter writer = response.getWriter();
-
+        getServletContext().getRequestDispatcher("/users/add.jsp")
+                .forward(request, response);
     }
 
     @Override
-    protected void doPost(HttpServletRequest request,
-                          HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        request.setCharacterEncoding("utf-8");
-        response.setCharacterEncoding("utf-8");
-        response.setContentType("text/html");
+        String userName = request.getParameter("userName");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
 
-        PrintWriter writer = response.getWriter();
+        UserDao userDao = new UserDao();
+        User user = new User();
+        user.setUsername(userName);
+        user.setEmail(email);
+        user.setPassword(password);
+        User createdUser = userDao.create(user);
+        if (createdUser != null) {
+        request.setAttribute("users", userDao.findAll());
+            getServletContext().getRequestDispatcher("/users/list.jsp")
+                    .forward(request, response);
+        } else {
+            response.getWriter().append("Problem z tworzeniem użytkownika");
+        }
+
 
     }
-
 }
